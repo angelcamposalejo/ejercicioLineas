3c2126c63099656f19e8a246a5f81b51279835cb
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-22 10:57:06 -0500

    version con piramide completa

--- MyLine/MyLine.cpp
+++ MyLine/MyLine.cpp
@@ -13,7 +13,17 @@ void MyLine::Window_Open(Win::Event& e)
 void MyLine::line(int asterisco)
 {
 	wstring texto;
-	for (int i = asterisco; i >= 1; i--)
+	for (int i = 1; i <= asterisco; i++)
+	{
+		for (int n = 1; i >= n; n++)
+		{
+			Sys::Format(texto, L"%s", "*");
+			tbxSalida.Text += texto;
+		}
+		Sys::Format(texto, L"\r\n");
+		tbxSalida.Text += texto;
+	}
+	for (int i = asterisco-1; i >= 1; i--)
 	{
 		for (int n = i; n >= 1; n--)
 		{
